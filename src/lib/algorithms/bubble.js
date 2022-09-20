1ffc6b9c375c8b5393effc8b8ef22d18ffcc5420
one-moonman <nicola.manoilov@gmail.com>
2022-09-20 14:04:25 +0200

    coloring added

--- src/lib/algorithms/bubble.js
+++ src/lib/algorithms/bubble.js
@@ -7,24 +7,37 @@ export default class BubbleSort {
         this.n = n;
     }
 
-    loop(pause, reframe) {
+    async loop(pause, reframe) {
         let i = this.i;
-        if (this.n == 1) return pause();
+        if (this.n == 1) {
+            array.colors[i] = "greenyellow";
+            return pause()
+        };
         if (i < this.n - 1) {
             const arr = [...array.values];
+
             array.colors[i] = "red";
+            //array.colors[i + 1] = "yellow";
+
             if (arr[i] > arr[i + 1]) {
                 const temp = arr[i];
                 arr[i] = arr[i + 1];
                 arr[i + 1] = temp;
-                array.colors[i] = "black";
+
+                //array.colors[i] = "yellow";
                 array.colors[i + 1] = "red";
+
                 array.values = [...arr];
             }
+
             array.colors[i - 1] = "black";
+            await reframe();
+            array.colors[i] = "black"
+
             this.i += 1;
-            return reframe();
+            return;
         }
+        array.colors[i] = "greenyellow"
 
         this.n -= 1;
         this.i = 0;
